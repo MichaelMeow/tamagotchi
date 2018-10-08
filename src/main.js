799fccf902a354618820f411f2db421924af2783
Hassan Al-khalifah and Michael Harmon <hassan@>
2018-10-08 10:43:56 -0700

    add UI, backend.js, basic styling of UI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import './styles.scss';
 
+// UI Logic
 $(document).ready(function() {
   console.log("hey, you should buy Abe a beer sometime!");
 });
